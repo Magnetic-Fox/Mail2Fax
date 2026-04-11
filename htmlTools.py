0549b876a9ce3e860ac4aebfc6b30c0adfc89e7e
Magnetic-Fox <magnetic-fox@hotmail.com>
2026-04-12 01:31:14 +0200

    Fix replacing strange <br ... > constructions to the new lines

--- htmlTools.py
+++ htmlTools.py
@@ -2,7 +2,7 @@
 
 # HTML tools for converting HTML to plain text
 #
-# by Magnetic-Fox, 13.07.2024 - 06.01.2026
+# by Magnetic-Fox, 13.07.2024 - 12.04.2026
 #
 # (C)2024-2026 Bartłomiej "Magnetic-Fox" Węgrzyn
 
@@ -18,8 +18,35 @@ class HTMLFilter(html.parser.HTMLParser):
 
 # Simple HTML to plain text converter
 def HTMLToText(inputData):
-	# Replace any <br> and <br /> to the new lines, because this simple HTMLFilter can't do this automatically
+	# Replace any <br> and <br /> (in all cases) to the new lines, because this simple HTMLFilter can't do this automatically
 	inputData = inputData.replace("<br>", "\n").replace("<br />", "\n")
+	inputData = inputData.replace("<Br>", "\n").replace("<Br />", "\n")
+	inputData = inputData.replace("<bR>", "\n").replace("<bR />", "\n")
+	inputData = inputData.replace("<BR>", "\n").replace("<BR />", "\n")
+
+	# Temporary variable for start position of <br ... > searching
+	posStart = 0
+
+	# Loop for searching strange <br ... > constructions...
+	while (inputData.find("<br ", posStart) != -1) or (inputData.find("<Br ", posStart) != -1) or (inputData.find("<bR ", posStart) != -1) or (inputData.find("<BR ", posStart) != -1):
+		posStart = inputData.find("<br ")
+		if posStart == -1:
+			posStart = inputData.find("<Br ")
+			if posStart == -1:
+				posStart = inputData.find("<bR ")
+				if posStart == -1:
+					posStart = inputData.find("<BR ")
+
+		# Search end position
+		endPos = inputData.find(">", posStart)
+
+		if endPos != -1:
+			# Replace all occurences and move to the beginning of the data
+			inputData = inputData.replace(inputData[posStart:endPos + 1], "\n")
+			posStart = 0
+		else:
+			# If '>' not found - move one character next to avoid inifite loop
+			posStart += 1
 
 	# Prepare converter and feed it with input data
 	converter = HTMLFilter()
